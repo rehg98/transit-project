a574bd2d4d3556e81a50bb4e8c730d36cf28d584
Ryan Golant <rgolant@princeton.edu>
2017-06-16 15:11:01 -0400

    added varying functions

--- transit.py
+++ transit.py
@@ -32,13 +32,44 @@ def trapezoid(pars, t):
 	temp[mask] = 0
 	return temp
 
+def vary_depth(depths):
+	t = np.linspace(-2., 2., 1000)
+	for d in depths:
+	 plt.plot(t, trapezoid([0., 1., 0.2, d], t))
+	plt.show()
+
+def vary_duration(durations):
+	t = np.linspace(-2., 2., 1000)
+	for d in durations:
+	 plt.plot(t, trapezoid([0., d, 0.2, 200], t))
+	plt.show()
+
+def vary_tau(taus):
+	t = np.linspace(-2., 2., 1000)
+	for tau in taus:
+	 plt.plot(t, trapezoid([0., 1., tau, 200], t))
+	plt.show()
+
+def vary_t0(t0s):
+	t = np.linspace(-2., 2., 1000)
+	for t0 in t0s:
+	 plt.plot(t, trapezoid([t0, 1., 0.2, 200], t))
+	plt.show()
+
+
+
+
+
 if __name__ == "__main__":
     #plot_data(sys.argv[1])
+    """
     pars = [0, 1.0, 0.2, 200] # t0 [center], T [duration], tau [ingress duration], depth
     t,f = read_data(7016.01)
     plt.plot(t, trapezoid(pars,t))
     plt.ylim(-250,50)
     plt.show()
+    """
+    
 
 
 
